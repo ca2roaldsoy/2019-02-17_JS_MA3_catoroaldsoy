0e7dd1aeb94fbc49bf95081ea398895eb0b3d429
ca2roaldsoy <44968868+ca2roaldsoy@users.noreply.github.com>
2019-02-17 19:32:35 +0100

    Initial Commit
    Assignment 2 - Level 1

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-//Use RegEx to validate form
-document.getElementById("submit").addEventListener("click", function() {
-                    
-    var userInput = document.getElementsByTagName("input");
-                
-        for(var i=0; i < userInput.length; i++){
-            var userValue = userInput[i].value.trim();
-            var userRegEx = new RegExp (userInput[i].pattern);
-            var userName = userRegEx.test(userValue);
-                    
-        if(userName == ""){
-            console.log(userInput[i].id + " is invalid")
-                
-        } else {
-            console.log (userValue)
-                
-            } 
-        }
-    })
+//Console.log out elements in JSON file
+var theAuthor = {
+	"video": [{
+		"id": 12312412312,
+		"name": "Ecuaciones Diferenciales",
+		"url": "/video/math/edo/12312412312",
+		"author": {
+			"data": [{
+				"name_author": "Alejandro Morales",
+				"url": "/author/alejandro-morales",
+				"type": "master"
+			}]
+		}
+	}]
+};
+         
+for(var i=0; i < theAuthor.video.length; i++) {
+    
+  var myAuthor = theAuthor.video[i]
+}
+ 
+    console.log(myAuthor)
+
+            
